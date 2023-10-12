9665d9d6b612ca97609392fdc9ab3f286b7f11fb
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2023-10-12 16:35:02 +0200

    add message for control e-mail

--- JS/script.js
+++ JS/script.js
@@ -16,21 +16,19 @@ let submit = document.querySelector('button');
 submit.addEventListener('click',
     function () {
         let mail = document.getElementById('mail').value.toLowerCase();
-        let validMail,invalidMail;
+        let validMail = 0;
+        let mailHelp = document.getElementById('emailHelp');
         for (let i = 0; i <= (mailList.length - 1); i++) {
             let listedMail = mailList[i];
-            console.log(listedMail);
-            if (mail !== listedMail) {
-                invalidMail = ('mail non valida');
+            if (mail === listedMail) {
+                mailHelp.classList += 'text-succes';
+                mailHelp.innerHTML=`E-mail valida`;
             }
-            else {
-                validMail = ('mail valida');
-            }
-
         }
-        console.log(validMail);
-
-
+        if(validMail === 0){
+            mailHelp.classList += 'text-danger';
+            mailHelp.innerHTML=`E-mail non registrata`;
+        }
     }
 );
 
